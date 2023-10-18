d4086aea9363de0c72c61286cbf1bdf89b34ec42
Bootylegg <114611705+Bootylegg@users.noreply.github.com>
2023-10-18 10:24:59 +0200

    innitial build

---
+++ index.js
@@ -0,0 +1,18 @@
+let change = document.getElementById("header")
+let pcparts = document.getElementById("parts")
+
+
+function changeScr() {
+    document.getElementById("mainimg").src="pics/image.png";
+  }
+function showAlert() {
+    alert ("Friend mode activated");
+  };
+
+function changeTitle() {
+    header.innerHTML = "Hello 👋";
+  };
+
+  function picker() {
+    parts.innerHTML = "My pc specs"
+  }
