9bae1e61fafbaa73c904469df45e25e2db4d9e55
Bootylegg <114611705+Bootylegg@users.noreply.github.com>
2023-10-18 13:53:30 +0200

    made logbook js :3

---
+++ logbook.js
@@ -0,0 +1,75 @@
+let planma = document.getElementById("logplanma");
+let donema = document.getElementById("logdonema");
+let plandi = document.getElementById("logplandi");
+let donedi = document.getElementById("logdonedi");
+let planwo = document.getElementById("logplanwo");
+let donewo = document.getElementById("logdonewo");
+let plando = document.getElementById("logplando");
+let donedo = document.getElementById("logdonedo");
+let planvr = document.getElementById("logplanvr");
+let donevr = document.getElementById("logdonevr");
+
+function week42() {
+    planma.innerHTML = "I am planning to do this today";
+    donema.innerHTML = "this is what I did today";
+    plandi.innerHTML = "I am planning to do this today";
+    donedi.innerHTML = "this is what I did today";
+    planwo.innerHTML = "I am planning to do this today";
+    donewo.innerHTML = "this is what I did today";
+    plando.innerHTML = "I am planning to do this today";
+    donedo.innerHTML = "this is what I did today";
+    planvr.innerHTML = "I am planning to do this today";
+    donevr.innerHTML = "this is what I did today";
+}
+
+function week43() {
+    planma.innerHTML = "Im Different";
+    donema.innerHTML = "this is what I did today";
+    plandi.innerHTML = "I am planning to do this today";
+    donedi.innerHTML = "this is what I did today";
+    planwo.innerHTML = "I am planning to do this today";
+    donewo.innerHTML = "this is what I did today";
+    plando.innerHTML = "I am planning to do this today";
+    donedo.innerHTML = "this is what I did today";
+    planvr.innerHTML = "I am planning to do this today";
+    donevr.innerHTML = "this is what I did today";
+}
+
+function week44() {
+    planma.innerHTML = "Im Different";
+    donema.innerHTML = "this is what I did today";
+    plandi.innerHTML = "I am planning to do this today";
+    donedi.innerHTML = "this is what I did today";
+    planwo.innerHTML = "I am planning to do this today";
+    donewo.innerHTML = "this is what I did today";
+    plando.innerHTML = "I am planning to do this today";
+    donedo.innerHTML = "this is what I did today";
+    planvr.innerHTML = "I am planning to do this today";
+    donevr.innerHTML = "this is what I did today";
+}
+
+function week45() {
+    planma.innerHTML = "Im Different";
+    donema.innerHTML = "this is what I did today";
+    plandi.innerHTML = "I am planning to do this today";
+    donedi.innerHTML = "this is what I did today";
+    planwo.innerHTML = "I am planning to do this today";
+    donewo.innerHTML = "this is what I did today";
+    plando.innerHTML = "I am planning to do this today";
+    donedo.innerHTML = "this is what I did today";
+    planvr.innerHTML = "I am planning to do this today";
+    donevr.innerHTML = "this is what I did today";
+}
+
+function week46() {
+    planma.innerHTML = "Im Different";
+    donema.innerHTML = "this is what I did today";
+    plandi.innerHTML = "I am planning to do this today";
+    donedi.innerHTML = "this is what I did today";
+    planwo.innerHTML = "I am planning to do this today";
+    donewo.innerHTML = "this is what I did today";
+    plando.innerHTML = "I am planning to do this today";
+    donedo.innerHTML = "this is what I did today";
+    planvr.innerHTML = "I am planning to do this today";
+    donevr.innerHTML = "this is what I did today";
+}
