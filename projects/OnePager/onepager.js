bbaea94f35cbb8afe3e10693b0ad6a4c57f73ecf
Bootylegg <114611705+Bootylegg@users.noreply.github.com>
2023-12-10 21:14:21 +0100

    pager

--- projects/OnePager/onepager.js
+++ projects/OnePager/onepager.js
@@ -1,5 +1,4 @@
 let phone = document.getElementById("phone")
-var colorchange = document.getElementById("text")
 
 function removeelement() {
     phone.style.opacity = 0
@@ -9,91 +8,64 @@ function addelement() {
     phone.style.opacity = 1
 }
 
-// function coloredtext() {
-//     var colorchange = document.getElementById('text'); 
-//     var randomNumber = Math.floor(Math.random() * 10) + 1;
-
-//     switch(randomNumber) {
-//         case 1:
-//             colorchange.style.color = "red";
-//             break;
-//         case 2:
-//             colorchange.style.color = "blue";
-//             break;
-//         case 3:
-//             colorchange.style.color = "green";
-//             break;
-//                     case 3:
-//             colorchange.style.color = "green";
-//             break;
-//         case 4:
-//             colorchange.style.color = "yellow";
-//             break;
-//                 case 3:
-//             colorchange.style.color = "green";
-//             break;
-//                 case 3:
-//             colorchange.style.color = "green";
-//             break;
-//         case 5:
-//             colorchange.style.color = "pink";
-//             break;
-//     }
-// }
-
 let hovercheck = 0;
 
-function hover() {
-    hovercheck = 1
-    console.log(hovercheck)
-}
+let colorInterval;
 
-function offhover() {
-    hovercheck = 0
-    console.log(hovercheck)
-}
+function startColorLoop() {
+    let hovercheck = 0;
+    let colorchange = document.getElementById('text');
 
-// function colorloop() {  
-//     if (hovercheck = 1) {
-//         coloredtext
-//         console.log("i work")
-//     }   
-// }
-
-function colorloop() {
-    for (let loop = 0; hovercheck <5; hovercheck++) {
-        if(hovercheck < 5) {
-            console.log(hovercheck)
-            var colorchange = document.getElementById('text'); 
-    var randomNumber = Math.floor(Math.random() * 10) + 1;
-
-    switch(randomNumber) {
-        case 1:
-            colorchange.style.color = "red";
-            break;
-        case 2:
-            colorchange.style.color = "blue";
-            break;
-        case 3:
-            colorchange.style.color = "green";
-            break;
-                    case 3:
-            colorchange.style.color = "green";
-            break;
-        case 4:
-            colorchange.style.color = "yellow";
-            break;
+    colorInterval = setInterval(() => {
+        switch (hovercheck) {
+            case 0:
+                colorchange.style.color = "blue";
+                break;
+            case 1:
+                colorchange.style.color = "red";
+                break;
+                case 2:
+                colorchange.style.color = "green";
+                break;
                 case 3:
-            colorchange.style.color = "green";
-            break;
-                case 3:
-            colorchange.style.color = "green";
-            break;
-        case 5:
-            colorchange.style.color = "pink";
-            break;
-    }
+            default:
+                colorchange.style.color = "black";
+                break;
         }
+        hovercheck = (hovercheck + 1) % 3;
+    }, 100);
+}
 
-    }
+function startColorLoop2() {
+    let hovercheck = 0;
+    let colorchange2 = document.getElementById('text2');
+
+    colorInterval = setInterval(() => {
+        switch (hovercheck) {
+            case 0:
+                colorchange2.style.color = "blue";
+                break;
+            case 1:
+                colorchange2.style.color = "red";
+                break;
+                case 2:
+                colorchange2.style.color = "green";
+                break;
+                case 3:
+            default:
+                colorchange2.style.color = "black";
+                break;
+        }
+        hovercheck = (hovercheck + 1) % 3;
+    }, 100);
+}
+
+function stopColorLoop() {
+    clearInterval(colorInterval);
+    document.getElementById('text').style.color = "black";
+}
+
+function stopColorLoop2() {
+    clearInterval(colorInterval);
+    document.getElementById('text2').style.color = "black";
 }
