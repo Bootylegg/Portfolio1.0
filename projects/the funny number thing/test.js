59b82251e49779f4446d7e4e58868da8bcd52e29
Bootylegg <114611705+Bootylegg@users.noreply.github.com>
2024-04-05 21:36:04 +0200

    random thingie

--- projects/the funny number thing/test.js
+++ projects/the funny number thing/test.js
@@ -37,3 +37,25 @@ function test() {
         console.log("No duplicate numbers found.");
     }
 }
+
+let pInput = document.getElementById("passwordInput")
+let pOutput = document.getElementById("passwordGuess")
+
+var array = ['a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 
+               0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '!', '@'];
+
+function passwordGuess() {
+        let random1 = array[Math.floor(Math.random() * 45)]
+        let random2 = array[Math.floor(Math.random() * 45)]
+        let random3 = array[Math.floor(Math.random() * 45)]
+        let random4 = array[Math.floor(Math.random() * 45)]
+        let random5 = array[Math.floor(Math.random() * 45)]
+            console.log(random1)
+            pOutput.innerHTML = (random1 + random2 + random3 + random4 + random5)
+            randomTotal == random1 + random2 + random3 + random4 + random5
+            if(randomTotal = pInput.value) {
+                alert("guessed")
+            } else {
+                passwordGuess()
+            }
+}
